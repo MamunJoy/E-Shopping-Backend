398b1e22a29f1ff62a8a7251006efdc1327cbec5
MamunJoy <mamunjoy4291@gmail.com>
2022-09-12 20:54:10 +0600

    first commit

---
+++ details.js
@@ -0,0 +1,135 @@
+const products=[
+    {
+    Name: "Apple",
+    Image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWFjYm9vayUyMHByb3xlbnwwfHwwfHw%3D&w=1000&q=80",
+    Details:`Brand Apple Model MacBook Pro
+    Series : MacBook Pro
+    Processor : Intel Core I5 7th Gen
+    RAM : 12GB SSD : 512GB
+    Connectivity : Wi-Fi Standards Supported 802.11 Ac`,
+    Price: 160000,
+    Rating: 3.5
+},
+
+
+
+
+{
+    Name: "Asus",
+    Image: "https://www.notebookcheck.net/uploads/tx_nbc2/ASUS_D509DA-BR128_01.jpg",
+    Details: `Brand Asus
+    Series : VivoBook
+    Processor : Intel Core I5 7th Gen
+    RAM : 12GB SSD : 512GB
+    Connectivity : Wi-Fi Standards Supported 802.11 Ac`,
+    Price: 56500,
+    Rating: 3.5
+},
+
+
+
+{
+    Name: "Dell",
+    Image: "https://m.media-amazon.com/images/I/61-yln4IXjL._AC_SL1500_.jpg",
+    Details: `Brand Model Dell
+    Series : VivoBook
+    Processor : Intel Core I5 7th Gen
+    RAM : 12GB SSD : 512GB
+    Connectivity : Wi-Fi Standards Supported 802.11 Ac`,
+    Price: 75500,
+    Rating: 3.5
+},
+
+
+{
+    Name: "HP Pavvilone",
+    Image: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c06745046.png",
+    Details: `Brand Model Hp Pavvilon Pro
+    Series : ElitBook
+    Processor : Intel Core I5 7th Gen
+    RAM : 12GB SSD : 512GB
+    Connectivity : Wi-Fi Standards Supported 802.11 Ac`,
+    Price: 62000,
+    Rating: 3.5
+},
+
+
+
+{
+    Name: "Huawei-Mate-20-Pro",
+    Image: "https://www.mobiledokan.com/wp-content/uploads/2019/03/Huawei-Mate-20-Pro-Green.jpg",
+    Details: `Size:6.4 Inches
+    Resolution:1080 X 2340 Pixels
+    Protection Corning Gorilla Glass 3
+    PLATFORM OS Android 9.0 (Pie),Upgradable To Android 11,One UI 3.1
+    MEMORY Card Slot MicroSDXC(Dedicated Slot)
+    Internal:64GB 4GB RAM, 128GB 4GB RAM, 128GB 6GB RAM`,
+    Price: 160000,
+    Rating: 3.5
+},
+
+
+
+{
+    Name: "Iphone 12",
+    Image: "https://www.telefonika.com/wp-content/uploads/2021/09/Apple-iPhone-13.jpg",
+    Details: `Size:6.4 Inches
+    Resolution:1080 X 2340 Pixels
+    Protection Corning Gorilla Glass 3
+    PLATFORM OS Android 9.0 (Pie),Upgradable To Android 11,One UI 3.1
+    MEMORY Card Slot MicroSDXC(Dedicated Slot)
+    Internal:64GB 4GB RAM, 128GB 4GB RAM, 128GB 6GB RAM`,
+    Price: 96000,
+    Rating: 3.5
+},
+
+
+
+{
+    Name: "Samsung-Galaxy-A50",
+    Image: "https://www.mobilebd.co/wp-content/uploads/2020/08/Samsung-Galaxy-A50.jpg",
+    Details: `Size:6.4 Inches
+    Resolution:1080 X 2340 Pixels
+    Protection Corning Gorilla Glass 3
+    PLATFORM OS Android 9.0 (Pie),Upgradable To Android 11,One UI 3.1
+    MEMORY Card Slot MicroSDXC(Dedicated Slot)
+    Internal:64GB 4GB RAM, 128GB 4GB RAM, 128GB 6GB RAM
+    MAIN CAMERA: Triple 48 MP,F/2.0,26mm (Wide)`,
+    Price: 42000,
+    Rating: 3.5
+},
+
+
+
+
+{
+    Name: "Vivo-Y20A-Nebula-Blue",
+    Image: "https://khaleejitech.com/wp-content/uploads/2021/01/vivoy21-1.jpg",
+    Details: `Size:6.4 Inches
+    Resolution:1080 X 2340 Pixels
+    Protection Corning Gorilla Glass 3
+    PLATFORM OS Android 9.0 (Pie),Upgradable To Android 11,One UI 3.1
+    MEMORY Card Slot MicroSDXC(Dedicated Slot)
+    Internal:64GB 4GB RAM, 128GB 4GB RAM, 128GB 6GB RAM`,
+    Price: 26000,
+    Rating: 3.5
+},
+
+
+
+{
+    Name: "Xiaomi-Redmi-Note-9-Pro",
+    Image: "https://www.gizmochina.com/wp-content/uploads/2020/03/Xiaomi-Redmi-Note-9-Pro-Max-500x500.jpg",
+    Details: `Size:6.4 Inches
+    Resolution:1080 X 2340 Pixels
+    Protection Corning Gorilla Glass 3
+    PLATFORM OS Android 9.0 (Pie),Upgradable To Android 11,One UI 3.1
+    MEMORY Card Slot MicroSDXC(Dedicated Slot)
+    Internal:64GB 4GB RAM, 128GB 4GB RAM, 128GB 6GB RAM`,
+    Price: 36000,
+    Rating: 3.5
+}
+]
+products.forEach(product=>{
+    console.log(product)
+})
